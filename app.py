9f55a13c09ecbf8efbf99959b8bd0571e39fa622
Niki <Nikolay.Borisov@endava.com>
2022-10-30 22:05:37 +0200

    app.py verion 1

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def metrics():
 def hello():
     app.logger.info('Main request successfull')
 
-    return "Cloud Native - version: 2"
+    return "Cloud Native - version: 1"
 
 if __name__ == "__main__":
     ## stream logs to a file
